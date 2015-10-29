c8d0a45849fc8321d84fcdecb876f5e020aa26e1
Inal Kardanov <ir_kardanov@guu.ru>
2015-10-30 00:30:37 +0300

    Updated logo view

--- calendar.php
+++ calendar.php
@@ -58,10 +58,7 @@
 		   <div class="aside-inner">
 			   <nav data-sidebar-anyclick-close="" class="sidebar">
                    <div class="brand-name">
-                       <a href="/">
-                           <img src="app/img/logo_500.png" width="30" height="30">
-                           <span>Evendate</span>
-                       </a>
+                       <div class="logo"><a title="" href="#"><img src="app/img/logo_500.png"> Evendate</a></div>
                    </div>
                    <!-- END user info-->
 				   <ul class="nav">
