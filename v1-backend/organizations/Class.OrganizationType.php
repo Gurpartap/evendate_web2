3603313b6c6a66b8747108a0276fa6317e63819f
kardanovir <kardinal3094@gmail.com>
2016-02-07 00:44:52 +0300

    Added organizations field for types Returned monolog for mongodb logging Fixed problem with test config

--- v1-backend/organizations/Class.OrganizationType.php
+++ v1-backend/organizations/Class.OrganizationType.php
@@ -8,6 +8,8 @@ class OrganizationType extends AbstractEntity{
 	protected $updated_at;
 	protected $order_position;
 
+	const ORGANIZATIONS_FIELD_NAME = 'organizations';
+
 	protected static $DEFAULT_COLS = array(
 		'id',
 		'name',
@@ -18,6 +20,23 @@ class OrganizationType extends AbstractEntity{
 		'created_at',
 		'updated_at'
 	);
+
+	public function getParams(User $user, array $fields = null) : Result {
+		$result_data = parent::getParams($user, $fields)->getData();
+
+		if (isset($fields[self::ORGANIZATIONS_FIELD_NAME])){
+			$result_data[self::ORGANIZATIONS_FIELD_NAME] =
+			OrganizationsCollection::filter(
+				App::DB(),
+				$user,
+				array('type_id' => $this->id),
+				Fields::parseFields($fields[self::ORGANIZATIONS_FIELD_NAME]['fields'] ?? ''),
+				$fields[self::ORGANIZATIONS_FIELD_NAME]['pagination'] ?? array(),
+				$fields[self::ORGANIZATIONS_FIELD_NAME]['order_by'] ?? array()
+			)->getData();
+		}
+		return new Result(true, '', $result_data);
+	}
 
 
 }
