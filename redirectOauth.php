715c94a9fcf7ae31582c50197a08e1b4cdfaf990
kardanovir <kardinal3094@gmail.com>
2016-05-24 22:20:36 +0300

    Fixed error with FB authorization

---
+++ redirectOauth.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Evendate - Авторизация</title>
+    <!-- =============== VENDOR STYLES ===============-->
+</head>
+
+<body>
+
+<?php
+require_once 'footer.php';
+?>
+<script>
+    $(document).ready(function () {
+        var data = $.extend(searchToObject(), hashToObject(), true);
+        var link = '/oAuthDone.php?',
+            params = [];
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                params.push(key + '=' + data[key]);
+            }
+        }
+        window.opener.location.href = link + params.join('&');
+    });
+
+
+</script>
