be025d53674633cc23967c7c97a2d5148434f3e9
kardanovir <kardinal3094@gmail.com>
2016-05-25 07:51:24 +0300

    Texts on landing changed

--- organizations.php
+++ organizations.php
@@ -10,8 +10,8 @@ require_once('landing/header.php'); ?>
 <div class="fon_img_main_page org_mini">
 	<div class="position_in_fon">
 		<div class="fon_users_text_container_org">
-			<div><span class="fon_text_top">Присоединяйтесь к нам!</span></div>
-			<div class="fon_text_bottom_container"><span class="fon_text_bottom">Evendate - для организаторов, кому нужно информировать о событии и анализировать отдачу.</span></div>
+			<div><span class="fon_text_top">Привлекайте и удерживайте целевую аудиторию</span></div>
+			<div class="fon_text_bottom_container"><span class="fon_text_bottom">Публикуйте события, общайтесь с участниками, находите потенциальных клиентов,  получайте аналитику по проведенным мероприятиям.</span></div>
 			<div>
 				<div id="join_button" class="button_fon_img_users_container_org"><button class="button_fon_img_users"><span class="text_button_users_top">ПРИСОЕДИНИТЬСЯ</span></button></div>
 				<a href="contacts.php#support" class="button_fon_img_users_container_org"><button class="button_fon_img_users button_fon_img_users_bot_users"><span class="text_button_users_bottom">ЗАДАТЬ ВОПРОС</span></button></a>
@@ -22,33 +22,32 @@ require_once('landing/header.php'); ?>
 
 
 <article class="article_fon_users">
-	<section class="section_stay_in_touch">
-		<div><span class="section_stay_in_touch_text_top">Что мы предлагаем?</span></div>
-		<div class="section_stay_in_touch_text_bottom_container"><span class="section_stay_in_touch_text_bottom">Наше приложение позволяет публиковывать мероприятия, оповещать о них, создавать аудиторию, следить за конкурентами, а также помагает в планировании.</span></div>
-	</section>
+<!--	<section class="section_stay_in_touch">-->
+<!--		<div><span class="section_stay_in_touch_text_top">Что мы предлагаем?</span></div>-->
+<!--		<div class="section_stay_in_touch_text_bottom_container"><span class="section_stay_in_touch_text_bottom">Наше приложение позволяет публиковывать мероприятия, оповещать о них, создавать аудиторию, следить за конкурентами, а также помагает в планировании.</span></div>-->
+<!--	</section>-->
 	<section class="fon_users_section">
 
 		<div class="steps_user_container">
 			<div class="step_user_container"><img src="/landing/img/9999.png" class="step_user_img"></div><div class="user_step_content_container">
-			<div><span class="step_text">ПЛОЩАДКА ДЛЯ СОБЫТИЙ</span></div>
-			<div class="step_user_text_bot_container"><span class="step_user_text">EVENDATE ПОЗВОЛЯЕТ БЫСТРО И УДОБНО ДОБАВЛЯТЬ СОБЫТИЯ. НАШ СЕРВИС ПРЕДОСТАВЛЯЕТ ВОЗМОЖНОСТЬ ИСПОЛЬЗОВАНИЯ КРОССПОСТИНГА.</span></div>
+			<div><span class="step_text">Площадка для событий</span></div>
+			<div class="step_user_text_bot_container"><span class="step_user_text">Публикуйте события, отправляйте уведомления, привлекайте участников. Новость о мероприятии автоматически размещается в сообществах в социальных сетях.</span></div>
 		</div>
 		</div>
 		<div class="line_blue_and_pink"></div>
 
 		<div class="steps_user_container">
 			<div class="user_step_content_container user_step_content_container_2">
-				<div><span class="step_text">АУДИТОРИЯ</span></div>
-				<div class="step_user_text_bot_container"><span class="step_user_text">EVENDATE - САМЫЙ ПРОСТОЙ СПОСОБ ПРИВЛЕЧЬ И УДЕРЖАТЬ НУЖНУЮ АУДИТОРИЮ.
-НАШИМ ПРИЛОЖЕНИЕМ ПОЛЬЗУЮТСЯ ТЫСЯЧИ СТУДЕТНОВ МОСКВЫ.</span></div>
+				<div><span class="step_text">Активная аудитория</span></div>
+				<div class="step_user_text_bot_container"><span class="step_user_text">Сервисом пользуются тысячи студентов и предпринимателей Москвы. Создайте сообщество и привлекайте участников на мероприятия.</span></div>
 			</div><div class="step_user_container_2"><img src="/landing/img/1010.png" class="step_user_img"></div>
 		</div>
 		<div class="line_blue_and_pink"></div>
 
 		<div class="steps_user_container">
 			<div class="step_user_container"><img src="/landing/img/1212.png" class="step_user_img"></div><div class="user_step_content_container">
-			<div><span class="step_text">СТАТИСТИКА И АНАЛИТИКА</span></div>
-			<div class="step_user_text_bot_container"><span class="step_user_text">ВОЗМОЖНОСТИ EVENDATE ОБШИРНЫ - ВЫ, КАК ОРГАНИЗАТОР, ВСЕГДА СМОЖЕТЕ ПОЛУЧИТЬ СТАТИСТИКУ ПРОВЕДЕННЫХ СОБЫТИЙ И АНАЛИЗИРОВАТЬ ПОЛУЧЕННЫЕ ДАННЫЕ. </span></div>
+			<div><span class="step_text">Статистика и аналитика</span></div>
+			<div class="step_user_text_bot_container"><span class="step_user_text">Узнайте о своей аудитории больше. Давайте то, что интересно и чего ожидают. Планируйте деятельность на основе реальных данных по мероприятиям в вашей сфере – собираем и анализируем информацию, чтобы помочь вам стать лучше.</span></div>
 		</div>
 		</div>
 	</section>
