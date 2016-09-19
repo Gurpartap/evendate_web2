0077962f64f2a155a2baad76f47ce65a8af7c5c7
kardanovir <kardinal3094@gmail.com>
2016-09-19 22:43:45 +0300

    Implemented tags sorting https://trello.com/c/ZHkiRzus

--- v1-backend/tags/Class.Tag.php
+++ v1-backend/tags/Class.Tag.php
@@ -1,27 +1,33 @@
 <?php
 
-class Tag extends AbstractEntity{
+class Tag extends AbstractEntity
+{
 
-	protected $name;
-	protected $created_at;
-	protected $updated_at;
-	protected $events_count;
-	
-	const RANDOM_FIELD_NAME = 'random';
+    protected $name;
+    protected $created_at;
+    protected $updated_at;
+    protected $events_count;
 
-	protected static $DEFAULT_COLS = array(
-		'id',
-		'name',
-	);
+    const RANDOM_FIELD_NAME = 'random';
+    const EVENTS_COUNT_FIELD_NAME = 'events_count';
+    const USED_COUNT_FIELD_NAME = 'used_count';
 
-	protected static $ADDITIONAL_COLS = array(
-		'created_at',
-		'updated_at',
-		'events_count',
-		self::RANDOM_FIELD_NAME => '(SELECT created_at / (random() * 9 + 1)
+    protected static $DEFAULT_COLS = array(
+        'id',
+        'name',
+    );
+
+    protected static $ADDITIONAL_COLS = array(
+        'created_at',
+        'updated_at',
+
+        self::EVENTS_COUNT_FIELD_NAME => '(SELECT count(events_tags.tag_id)
+            FROM events_tags
+            WHERE events_tags.tag_id = view_tags.id) AS ' . self::EVENTS_COUNT_FIELD_NAME,
+        self::RANDOM_FIELD_NAME => '(SELECT created_at / (random() * 9 + 1)
 			FROM view_tags AS vt
-			WHERE vt.id = view_tags.id) AS random',
-	);
+			WHERE vt.id = view_tags.id) AS ' . self::RANDOM_FIELD_NAME,
+    );
 
 
 }
