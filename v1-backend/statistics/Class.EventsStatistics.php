67b7c08dbc76fcf4932f5f957b6e78b3d451450e
kardanovir <kardinal3094@gmail.com>
2016-09-23 00:10:35 +0300

    Event detail statistics

--- v1-backend/statistics/Class.EventsStatistics.php
+++ v1-backend/statistics/Class.EventsStatistics.php
@@ -5,39 +5,192 @@ require_once $BACKEND_FULL_PATH . '/statistics/Class.AbstractAggregator.php';
 class EventsStatistics extends AbstractAggregator
 {
 
+
+    const SQL_GET_DATA = '
+      SELECT
+      COUNT(stat_events.id) AS value,
+      DATE_PART(\'epoch\', ts.time_value)::INT AS time_value
+      FROM stat_events
+        INNER JOIN stat_event_types ON stat_events.stat_type_id = stat_event_types.id 
+          AND stat_event_types.entity = :entity
+          AND stat_event_types.type_code = :type_code
+          AND stat_events.event_id = :event_id
+        INNER JOIN events ON events.id = stat_events.event_id 
+        INNER JOIN tokens ON stat_events.token_id = tokens.id
+          AND tokens.user_id NOT IN (SELECT user_id
+            FROM users_organizations
+            WHERE users_organizations.organization_id = events.organization_id
+            AND users_organizations.status = TRUE)
+        RIGHT OUTER JOIN (SELECT *
+                    FROM generate_series(to_timestamp(:till), to_timestamp(:since), \'-1 {SCALE}\')) AS ts(time_value)
+                    ON stat_events.created_at BETWEEN (ts.time_value - INTERVAL \'1 {SCALE}\') AND ts.time_value
+        GROUP BY ts.time_value
+        ORDER BY ts.time_value
+        LIMIT 10000';
+
+    const SQL_GET_NOTIFICATIONS = 'SELECT
+      COUNT(stat_notifications.id) AS value,
+      DATE_PART(\'epoch\', ts.time_value)::INT AS time_value
+      FROM stat_notifications
+        INNER JOIN events_notifications ON stat_notifications.event_notification_id = events_notifications.id 
+        INNER JOIN events ON events_notifications.event_id = events.id AND events.id = :event_id
+        RIGHT OUTER JOIN (SELECT *
+                    FROM generate_series(to_timestamp(:till), to_timestamp(:since), \'-1 {SCALE}\')) AS ts(time_value)
+                    ON stat_notifications.created_at BETWEEN (ts.time_value - INTERVAL \'1 {SCALE}\') AND ts.time_value
+        GROUP BY ts.time_value
+        ORDER BY ts.time_value
+        LIMIT 10000';
+
+
     private $db;
     private $event;
 
-    const SQL_GET_VALUES = 'SELECT COUNT(stat_events.id) AS value
-      FROM stat_events
-      INNER JOIN stat_event_types ON stat_events.stat_type_id = stat_event_types.id
-      WHERE stat_events.created_at BETWEEN :since AND :till';
-
-    const SQL_GET_NOTIFICATIONS_SENT = 'SELECT COUNT("view_stat_notifications"."id") AS "value"
-      FROM view_stat_notifications 
-      WHERE view_stat_notifications.notification_time BETWEEN :since AND :till';
-
-    const SQL_GET_NOTIFICATIONS_SENT_AGGREGATED = 'SELECT notifications_count
-      FROM stat_notifications_aggregated 
-      WHERE 1 = 1 ';
-
-    const SQL_GET_USERS_NOTIFICATIONS = 'SELECT COUNT(users_notifications.id) AS value
-      FROM users_notifications
-      WHERE users_notifications.created_at BETWEEN :since AND :till';
-
-    const SQL_GET_VIEWS = self::SQL_GET_VALUES . '';
 
     public function __construct(PDO $db, Event $event, User $user)
     {
-        if (!$user->isEventAdmin($event)) throw new PrivilegesException('', $db);
+        if (!$user->isAdmin($event->getOrganization())) throw new PrivilegesException('', $db);
         $this->db = $db;
         $this->event = $event;
     }
-    
-    public static function getValueSQLWithNamedParams($type){
-        return self::SQL_GET_VALUES 
-        . ' AND stat_event_types.type_code = :type_code_' . $type 
-        . ' AND stat_events.event_id = view_events.id';
-    } 
+
+
+    private function getValue($type, $scale, DateTime $since, DateTime $till)
+    {
+        switch ($type) {
+            case Statistics::EVENT_VIEW:
+            case Statistics::EVENT_VIEW_DETAIL:
+            case Statistics::EVENT_FAVE:
+            case Statistics::EVENT_UNFAVE:
+            case Statistics::EVENT_OPEN_SITE: {
+                $query = str_replace('{SCALE}', $scale, self::SQL_GET_DATA);
+                $statements = array(
+                    ':event_id' => $this->event->getId(),
+                    ':entity' => Statistics::ENTITY_EVENT,
+                    ':type_code' => $type,
+                    ':since' => $since->getTimestamp(),
+                    ':till' => $till->getTimestamp(),
+                );
+                break;
+            }
+            case Statistics::EVENT_NOTIFICATIONS_SENT: {
+                $query = str_replace('{SCALE}', $scale, self::SQL_GET_NOTIFICATIONS);
+                $statements = array(
+                    ':event_id' => $this->event->getId(),
+                    ':since' => $since->getTimestamp(),
+                    ':till' => $till->getTimestamp(),
+                );
+                break;
+            }
+            default: {
+                throw new InvalidArgumentException();
+            }
+        }
+
+        $q_get_data = $this->db->prepare($query);
+
+        $q_get_data->execute($statements);
+
+        return new Result(true, '', $q_get_data->fetchAll());
+    }
+
+    public function get(array $fields, $scale, DateTime $since, DateTime $till)
+    {
+        $result = array();
+        foreach ($fields as $key => $value) {
+            switch ($key) {
+                case Statistics::FIELD_DYNAMICS: {
+                    $result[Statistics::FIELD_DYNAMICS] = $this->getDynamics(
+                        Fields::parseFields($value['fields'] ?? ''),
+                        $value['scale'] ?? $scale,
+                        isset($value['since']) ? DateTime::createFromFormat('U', $value['since']) : $since,
+                        isset($value['till']) ? DateTime::createFromFormat('U', $value['till']) : $till
+                    )->getData();
+                    break;
+                }
+                case Statistics::FIELD_CONVERSION: {
+                    $result[Statistics::FIELD_CONVERSION] = $this->getConversion(
+                        $value['scale'] ?? $scale,
+                        isset($value['since']) ? DateTime::createFromFormat('U', $value['since']) : $since,
+                        isset($value['till']) ? DateTime::createFromFormat('U', $value['till']) : $till
+                    )->getData();
+                    break;
+                }
+                case Statistics::EVENT_NOTIFICATIONS_SENT:
+                case Statistics::EVENT_FAVE:
+                case Statistics::EVENT_UNFAVE:
+                case Statistics::EVENT_VIEW:
+                case Statistics::EVENT_VIEW_DETAIL:
+                case Statistics::EVENT_OPEN_SITE: {
+                    $result[$key] = $this->getValue(
+                        $key,
+                        $value['scale'] ?? $scale,
+                        isset($value['since']) ? DateTime::createFromFormat('U', $value['since']) : $since,
+                        isset($value['till']) ? DateTime::createFromFormat('U', $value['till']) : $till
+                    )->getData();
+                    break;
+                }
+            }
+        }
+        return new Result(true, '', $result);
+    }
+
+    private function getDynamics(array $fields = null, $scale, DateTime $since, DateTime $till)
+    {
+        $default_fields = array(
+            Statistics::ORGANIZATION_SUBSCRIBE => null,
+            Statistics::ORGANIZATION_VIEW => null,
+            Statistics::EVENT_FAVE => null
+        );
+
+        if (!is_array($fields) || (is_array($fields) && count($fields) == 0)) {
+            $fields = $default_fields;
+        }
+        $result = array();
+
+        foreach ($fields as $key => $param) {
+            switch ($key) {
+                case Statistics::ORGANIZATION_VIEW:
+                case Statistics::EVENT_FAVE:
+                case Statistics::ORGANIZATION_SUBSCRIBE: {
+                    $result[$key] = $this->getValue($key, $scale, $since, $till)->getData();
+                    break;
+                }
+                case Statistics::FIELD_CONVERSION: {
+                    $result[Statistics::FIELD_CONVERSION] = $this->getConversion($scale, $since, $till)->getData();
+                    break;
+                }
+            }
+        }
+        return new Result(true, '', $result);
+    }
+
+    private function getConversion($scale, $since, $till)
+    {
+        $views = $this->getValue(
+            Statistics::EVENT_VIEW,
+            $scale,
+            $since,
+            $till
+        )->getData();
+
+        $subscribes = $this->getValue(
+            Statistics::EVENT_FAVE,
+            $scale,
+            $since,
+            $till
+        )->getData();
+
+        $result = array();
+
+        foreach ($views as $key => $view) {
+            $result[] = array(
+                'time_value' => $view['time_value'],
+                'subscribe' => $subscribes[$key]['value'],
+                'view' => $view['value'],
+                'value' => $view['value'] == 0 ? 0 : $subscribes[$key]['value'] / $view['value'] * 100
+            );
+        }
+        return new Result(true, '', $result);
+    }
     
 }
