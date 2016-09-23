c02409fd74af17610c2fc7fb309eed3444e2556e
kardanovir <kardinal3094@gmail.com>
2016-09-23 16:30:18 +0300

    Fixed overall Added fave_conversion, open_conversion for event

--- v1-backend/statistics/Class.AbstractAggregator.php
+++ v1-backend/statistics/Class.AbstractAggregator.php
@@ -4,5 +4,12 @@ class AbstractAggregator
 {
 
 
+    protected function replaceScale($query, $scale){
+        if (mb_strtolower($scale) == 'overall'){
+            $scale = 'century';
+        }
+        $query = str_replace('{SCALE}', $scale, $query);
+        return $query;
+    }
 
 }
