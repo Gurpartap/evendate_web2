860dcfb683e4d0c715f7d845350e8d81d2ba072f
Kharazyan Aram <armxaz@gmail.com>
2016-11-16 20:19:22 +0300

    Fixed bug https://trello.com/c/gqzsQDPm

--- index.php
+++ index.php
@@ -496,7 +496,7 @@ $title = 'Evendate';
 
 <footer class="footer fluid-width mobile-full-width">
 
-    <a href="index.html" data-ga-label="DownArrow" class="down-arrow hover-fill js-ga-track">
+    <a href="#" data-ga-label="DownArrow" class="down-arrow hover-fill js-ga-track">
         <svg width="30px" height="30px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
              xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 60 60"
              enable-background="new 0 0 60 60" xml:space="preserve">
