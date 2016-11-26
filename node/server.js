73257257b3b8c5f5e501a57bfb3367be9bffc989
kardanovir <kardinal3094@gmail.com>
2016-11-26 17:30:27 +0300

    Fixed error with authorization[3]

--- node/server.js
+++ node/server.js
@@ -463,7 +463,7 @@ pg.connect(pg_conn_string, function (err, client, done) {
                             .join(Entities.vk_users_subscriptions).on(Entities.vk_users_subscriptions.vk_group_id.equals(Entities.vk_groups.id))
                     ).where(Entities.vk_users_subscriptions.user_id.equals(user_id)).toQuery(),
                     cleanData = function (str) {
-                        if (typeof str != 'string') return '';
+                        if (typeof str !== 'string') return '';
                         str = str.replace(/<[^>]*>/gmi, ''); // remove tags
                         str = str.replace(/[^a-zA-Zа-яА-ЯёЁ\-\s]/gmi, ''); // remove NOT words
                         var str_items = str.split(/\s/gmi),
@@ -496,17 +496,9 @@ pg.connect(pg_conn_string, function (err, client, done) {
 
                         groups_data.rows.forEach(function (row) {
 
-                            if (typeof row[type] == 'string'){
                                 items.push(cleanData(row.name));
-                            }
-
-                            if (typeof row[type] == 'string'){
                                 items.push(cleanData(row.screen_name));
-                            }
-
-                            if (typeof row[type] == 'string'){
                                 items.push(cleanData(row.description));
-                            }
                         });
                         var items_text = items.join(' '),
                             items_text_tsquery = items_text.trim().replace(/\s+/gmi, '|');
