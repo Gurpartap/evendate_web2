a373f888885e0bc3ca9d1fad0d34fd6035e022c3
kardanovir <kardinal3094@gmail.com>
2017-03-15 23:43:04 +0300

    Fixes issue with Events?fields=ticket https://trello.com/c/63qe8aHv

--- landing/js/demo.js
+++ landing/js/demo.js
@@ -1,18 +1,13 @@
-//Ultima HTML5 Landing Page v2.3
-//Copyright 2014 8Guild.com
-//All scripts for Ultima Demo Page
-
-/*Page Preloading*/
-$(window).load(function() {
-	$('#spinner').fadeOut();
-	$('#preloader').delay(300).fadeOut('slow');
-	setTimeout(function(){$('.mac-left img').addClass('fadeInLeft');},100);
-	setTimeout(function(){$('.mac-right img').addClass('fadeInRight');},100);
-	setTimeout(function(){$('.mac-left a').addClass('bounceInDown');},100);
-	setTimeout(function(){$('.mac-right a').addClass('bounceInDown');},100);
-});
-
-/*Checking if it's touch device we disable some functionality due to inconsistency*/
-if (Modernizr.touch) { 
-	$('*').removeClass('animated');
-}
+var a = {
+    "app_id": "7471a586-01f3-4eef-b989-c809700a8658",
+    "data": {
+        "payload": {"icon": "https://evendate.ru/organizations_images/logos/small/9c58b4ce31bd53494e7ab7d130f8a867.png"},
+        "contents": {
+            "en": "Осталось 2 часа до события \"Второй этап лиги КВН ГУУ\"",
+            "ru": "Mail.Ru Group: GeekDay: 2 Evolution"
+        },
+        "url": "https://evendate.ru/a/poll",
+        "isAndroid": true,
+        "include_player_ids": ["9fcb9462-2cb0-4182-9402-ebd39ca4551a"]
+    }
+};
