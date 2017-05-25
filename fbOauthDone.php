6a49f3f91b87d107b09a9b20c488dfc5360fb9a8
kardanovir <kardinal3094@gmail.com>
2017-05-25 17:33:08 +0300

    Fixes issue with favorites in private organizations https://trello.com/c/fnL0uSP3

--- fbOauthDone.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<script>
-    window.location.href = '/redirectOauth.php?type=facebook&' + window.location.search.substring(1) + window.location.hash;
-</script>
-</body>
-</html>
--- googleOauthDone.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<script>
-    window.location.href = '/redirectOauth.php?type=google&' + window.location.search.substring(1) + window.location.hash;
-</script>
-</body>
-</html>
