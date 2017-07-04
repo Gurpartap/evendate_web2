7c3a38c3f005896dfe431d0ee9ef3e8fd76ec12d
Kharazyan Aram <aa.harazyan@guu.ru>
2017-07-04 17:42:11 +0300

    Added QuantityInput component

--- app/src/js/ui/Class.Builder.js
+++ app/src/js/ui/Class.Builder.js
@@ -108,10 +108,11 @@ Builder = (function() {
 	 * @param {HTMLAttributes} [attributes]
 	 * @param {(Array<string>|string)} [classes]
 	 * @param {HTMLDataset} [dataset]
+	 * @param {object} [inputmask_options]
 	 *
 	 * @returns {jQuery}
 	 */
-	Builder.prototype.inputNumber = function buildInput(attributes, classes, dataset) {
+	Builder.prototype.inputNumber = function buildInput(attributes, classes, dataset, inputmask_options) {
 		attributes = attributes ? attributes : {};
 		classes = classes ? classes instanceof Array ? classes : classes.split(',') : [];
 		dataset = dataset ? dataset : {};
@@ -120,7 +121,7 @@ Builder = (function() {
 			attributes,
 			classes.concat('form_input'),
 			dataset
-		).inputmask({
+		).inputmask($.extend({
 			alias: 'numeric',
 			autoGroup: false,
 			digits: 2,
@@ -128,7 +129,7 @@ Builder = (function() {
 			allowPlus: false,
 			allowMinus: false,
 			rightAlign: false
-		});
+		}, inputmask_options));
 	};
 	/**
 	 *
