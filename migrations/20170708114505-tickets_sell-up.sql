79f1aaf454a467c3cdd8fbe270256d5beec3452d
kardanovir <kardinal3094@gmail.com>
2017-07-10 17:04:50 +0300

    Fixes issue with order_number in admin  https://trello.com/c/coMATuoX

--- migrations/20170708114505-tickets_sell-up.sql
+++ migrations/20170708114505-tickets_sell-up.sql
@@ -366,3 +366,21 @@ CREATE OR REPLACE VIEW view_actions AS SELECT
                                          stat_organizations.stat_type_id, stat_event_types.name,
                                          stat_event_types.entity,
                                          stat_event_types.type_code;
+
+CREATE OR REPLACE VIEW view_registration_field_values AS
+  SELECT
+    view_registration_form_fields.uuid AS form_field_uuid,
+    view_registration_form_fields.label AS form_field_label,
+    view_registration_form_fields.type AS form_field_type,
+    view_registration_form_fields.field_type_id AS form_field_type_id,
+    view_registration_form_fields.required AS form_field_required,
+    registration_field_values.value,
+    registration_field_values.values::JSONB,
+    DATE_PART('epoch', registration_field_values.created_at) :: INT AS created_at,
+    DATE_PART('epoch', registration_field_values.updated_at) :: INT AS updated_at,
+    registration_field_values.ticket_order_id,
+    view_tickets_orders.uuid AS ticket_order_uuid,
+    view_registration_form_fields.order_number
+  FROM registration_field_values
+    INNER JOIN view_registration_form_fields ON registration_field_values.registration_form_field_id = view_registration_form_fields.id
+    INNER JOIN view_tickets_orders ON registration_field_values.ticket_order_id = view_tickets_orders.id;
